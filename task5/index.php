c90f50d4c67839f8f5d791c4885cb74a03f667eb
Rezyapkin <rezyapkin@yandex.ru>
2020-09-26 14:35:13 +0300

    complete task5

---
+++ task5/index.php
@@ -0,0 +1,14 @@
+<?
+$about = renderTemplate('about','Привет мир!');
+$menu = renderTemplate('menu');
+
+echo renderTemplate('main', $about, $menu);
+
+function renderTemplate($page, $content = "", $menu = "") {
+    ob_start();
+    $filename = $page . ".php";
+    if (file_exists($filename)) {
+        include $page . ".php";
+    }
+    return ob_get_clean();
+}
