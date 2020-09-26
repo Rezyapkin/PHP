d5027ca65cd318ab16c0cb4cc305322e1627757e
Rezyapkin <rezyapkin@yandex.ru>
2020-09-26 14:17:16 +0300

    complete task3

---
+++ task3.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Реализовать основные 4 арифметические операции в виде функций с двумя параметрами.
+Обязательно использовать оператор return. В делении проверьте деление на 0 и верните текст ошибки.
+*/
+
+function sum($x, $y) {
+    return $x + $y;
+}
+
+function sub($x, $y) {
+    return $x - $y;
+}
+
+function mul($x, $y) {
+    return $x * $y;
+}
+
+function dev($x, $y) {
+    return ($y == 0) ? "Деление на 0": $x / $y;
+}
+
+$x = rand(-10,10);
+$y = rand(-10,10);
+
+echo "x = {$x} <br>";
+echo "y = {$y} <br>";
+
+echo "x + y = ".sum($x,$y)."<br>";
+echo "x - y = ".sub($x,$y)."<br>";
+echo "x * y = ".mul($x,$y)."<br>";
+echo "x / y = ".dev($x,$y)."<br>";
